e0b596589276e5dece9755c455b0586b46fc5978
Alexey Sedov <ASedov15@gmail.com>
2023-05-05 18:47:53 +0300

    Updates

---
+++ precache-manifest.b8ed7c89b991be35d5041cd9824b30e3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "71b15aab9f73be10e7d4",
+    "url": "/react_blog/static/css/main.698206f7.chunk.css"
+  },
+  {
+    "revision": "71b15aab9f73be10e7d4",
+    "url": "/react_blog/static/js/main.71b15aab.chunk.js"
+  },
+  {
+    "revision": "5705f17706b8cb464864",
+    "url": "/react_blog/static/css/1.60edc314.chunk.css"
+  },
+  {
+    "revision": "5705f17706b8cb464864",
+    "url": "/react_blog/static/js/1.5705f177.chunk.js"
+  },
+  {
+    "revision": "038b941ff4ddf0aaf811",
+    "url": "/react_blog/static/js/runtime~main.038b941f.js"
+  },
+  {
+    "revision": "d0494a6fd9625c052284acab63eaf423",
+    "url": "/react_blog/static/media/400-1unsplash.d0494a6f.jpg"
+  },
+  {
+    "revision": "a53aceadba6a57285b1606fa594976a1",
+    "url": "/react_blog/static/media/photo400.a53acead.jpg"
+  },
+  {
+    "revision": "51dbb85f36644667b737b8e7dab0f76f",
+    "url": "/react_blog/index.html"
+  }
+];
